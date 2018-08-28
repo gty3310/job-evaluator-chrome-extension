fe95abd55f95428e106863a3bdf73ee14b1c61a5
gty3310 <guoty3310@gmail.com>
2018-08-28 10:29:04 -0700

    enable search with standalone keywords

--- content.js
+++ content.js
@@ -2,15 +2,17 @@ var elements = document.getElementsByTagName('*');
 
 var num = 0;
 
-var skillsList = ["javascript", "HTML", "extension", "Script"];
+var skillsList = ["react.js", "JavaScript", "script", "react"];
 
 //get the number of appearance of a keyword
 var wholeText =  document.body.innerText;
 var skillInReg = "";
 var skillsCount = 0;
 for (let listIndex = 0; listIndex < skillsList.length; listIndex ++){
-    skillInReg = new RegExp(skillsList[listIndex],'g');
+    // skillInReg = new RegExp(skillsList[listIndex],'g');
+    skillInReg = new RegExp("\\b"+skillsList[listIndex]+ "\\b",'gi');
     skillsCount += (wholeText.match(skillInReg) || []).length;
 }
 
-
+console.log("------skillsCount:");
+console.log(skillsCount);
