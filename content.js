9f51c6670b73268fe6fb5feeb490d2fca31b57e7
gty3310 <guoty3310@gmail.com>
2018-08-27 22:11:50 -0700

    enable counting multiple keywords

--- content.js
+++ content.js
@@ -1,8 +1,16 @@
 var elements = document.getElementsByTagName('*');
-console.log("right");
+
 var num = 0;
+
+var skillsList = ["javascript", "HTML", "extension", "Script"];
 
 //get the number of appearance of a keyword
 var wholeText =  document.body.innerText;
-var count = (wholeText.match(/is/g) || []).length;
+var skillInReg = "";
+var skillsCount = 0;
+for (let listIndex = 0; listIndex < skillsList.length; listIndex ++){
+    skillInReg = new RegExp(skillsList[listIndex],'g');
+    skillsCount += (wholeText.match(skillInReg) || []).length;
+}
+
 
