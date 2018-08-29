92b0959e28186265015edd809d85174255312904
alex <akeatk@gmail.com>
2018-08-29 11:10:16 -0700

    working search for user and all skills

--- content.js
+++ content.js
@@ -1,73 +1,154 @@
 console.log('running context.js')
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse){
+  (request, sender, sendResponse)=>{
 
     switch(request.command){
       case "findAllSkills":
+        const notChar = (c)=>{
+          return 'abcdefghijklmnopqrstuvwxyz'.indexOf(c) < 0 &&
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(c) < 0;
+        }
         //-----------------------------------------------------------
-        var keywords = request.keywords.sort((a,b)=>b.length - a.length);
-        var keywordCounter = {};
+        const allSkills = request.allSkills.sort((a,b)=>b.length - a.length);
+        const userSkills = request.userSkills.sort((a,b)=>b.length - a.length);
+        let userCounter = {};
+        let allCounter = {};
+        console.log('find all skills');
+        console.log(allSkills);
+        console.log(userSkills);
 
-        var reader = document.body.innerHTML;
-        var setter = document.body.innerHTML;
+        let reader = document.body.innerHTML;
+        let setter = document.body.innerHTML;
 
-        var inCode = false;
-        var inQuotes = false;
-        var ridx = 0;
-        var sidx = 0;
+        let inCode = false;
+        let inQuotes = false;
+        let ridx = 0;
+        let sidx = 0;
 
         while(ridx < reader.length){
           // makes sure keyword isn't in html code
-          if(reader[i]==="<" && !inQuotes)
+          // if(inCode && "`'\"".indexOf(reader[i]) > -1)
+          //   inQuotes = !inQuotes;
+          // else
+          if(reader[ridx]==="<" && !inQuotes)
             inCode = true;
-          else if(reader[i]===">" && !inQuotes)
+          else if(reader[ridx]===">" && !inQuotes)
             inCode = false;
-          else if("`'\"".indexOf(reader[i]) > -1)
-            inQuotes = !inQuotes;
 
           // scans for keywords by checking strong + 1 char before and after
+          let found = false;
           if(!inCode){
-            for(let i = 0;i < keywords.length;i++){
-              skillInReg = new RegExp("\\b"+keywords[i]+ "\\b",'gi');
-              skillsCount += (wholeText.match(skillInReg) || []).length;
+            // countAndReplace(userCounter,)
+            for(let i = 0;i < userSkills.length;i++){
 
+              // checks if the keyword is in the beginning of the string
+              let substring = reader.substring(ridx, ridx + userSkills[i].length);
+              if(substring.toLowerCase() === userSkills[i].toLowerCase() &&
+                  notChar(reader[ridx - 1]) &&
+                  notChar(reader[ridx + userSkills[i].length + 1])){
+                // increase counter or set counter to 1 for skill
+                if(userCounter[userSkills[i]])
+                  userCounter[userSkills[i]] = 1;
+                else
+                  userCounter[userSkills[i]] += 1;
+
+                if(allCounter[userSkills[i]])
+                  allCounter[userSkills[i]] = 1;
+                else
+                  allCounter[userSkills[i]] += 1;
+
+                ridx += userSkills[i].length - 1;
+
+                // insert first half of highligh html
+                const highlight1=`<cake style="background-color:#00ffcc;">`;
+                setter = setter.slice(0,sidx)
+                    + highlight1
+                    + setter.slice(sidx);
+                sidx += highlight1.length + userSkills[i].length;
+
+                const highlight2=`</cake>`;
+                setter = setter.slice(0,sidx)
+                    + highlight2
+                    + setter.slice(sidx);
+                sidx += highlight2.length - 1;
+
+                found = true;
+                break;
+              }
+            }
+
+            if(!found){
+              for(let i = 0;i < allSkills.length;i++){
+                // checks if the keyword is in the beginning of the string
+                let substring = reader.substring(ridx, ridx + allSkills[i].length);
+                if(substring.toLowerCase() === allSkills[i].toLowerCase() &&
+                    notChar(reader[ridx - 1]) &&
+                    notChar(reader[ridx + allSkills[i].length + 1])) {
+                  // increase counter or set counter to 1 for keyword
+                  if(allCounter[allSkills[i]])
+                    allCounter[allSkills[i]] = 1;
+                  else
+                    allCounter[allSkills[i]] += 1;
+
+                  ridx += allSkills[i].length - 1;
+
+                  // insert first half of highligh html
+                  const highlight1="<cake style='background-color:#ffaa80;'>";
+                  setter = setter.slice(0,sidx)
+                      + highlight1
+                      + setter.slice(sidx);
+                  sidx += highlight1.length + allSkills[i].length;
+
+                  const highlight2="</cake>";
+                  setter = setter.slice(0,sidx)
+                      + highlight2
+                      + setter.slice(sidx);
+                  sidx += highlight2.length - 1;
+
+                  break;
+                }
+              }
             }
           }
+          ridx++;
+          sidx++;
         }
-        //-----------------------------------------------------------
-        var skillsList = ["react.js", "JavaScript", "react", 'retrieve'];
-        var userSkillsList = [ "JavaScript"];
-
-        //get the number of appearances of a keyword
-        var wholeText =  document.body.innerText;
-        var skillInReg = "";
-        var skillsCount = 0;
-        var userSkillsCount = 0;
-        for (let listIndex = 0; listIndex < skillsList.length; listIndex ++){
-          skillInReg = new RegExp("\\b"+skillsList[listIndex]+ "\\b",'gi');
-          skillsCount += (wholeText.match(skillInReg) || []).length;
-
-          //add highlight to words
-          var replaceText = "<cake style='background-color:red;'>"
-                            + skillsList[listIndex]
-                            + "</cake>";
-          var wholeHTML = document.body.innerHTML;
-          var newHTML = wholeHTML.replace(skillInReg, replaceText);
-          if(wholeHTML !== newHTML)
-            document.body.innerHTML = newHTML;
-        }
-        for (let listIndex = 0; listIndex < userSkillsList.length; listIndex ++){
-            skillInReg = new RegExp("\\b"+userSkillsList[listIndex]+ "\\b",'gi');
-            userSkillsCount += (wholeText.match(skillInReg) || []).length;
-        }
-
-        console.log("------skillsCount:");
-        console.log(skillsCount);
-        console.log("------userSkillsCount:");
-        console.log(userSkillsCount);
-
+        document.body.innerHTML = setter;
         break;
+        //-----------------------------------------------------------
+        // var skillsList = ["react.js", "JavaScript", "react", 'retrieve'];
+        // var userSkillsList = [ "JavaScript"];
+        //
+        // //get the number of appearances of a keyword
+        // var wholeText =  document.body.innerText;
+        // var skillInReg = "";
+        // var skillsCount = 0;
+        // var userSkillsCount = 0;
+        // for (let listIndex = 0; listIndex < skillsList.length; listIndex ++){
+        //   skillInReg = new RegExp("\\b"+skillsList[listIndex]+ "\\b",'gi');
+        //   skillsCount += (wholeText.match(skillInReg) || []).length;
+        //
+        //   //add highlight to words
+        //   var replaceText = "<cake style='background-color:red;'>"
+        //                     + skillsList[listIndex]
+        //                     + "</cake>";
+        //   var wholeHTML = document.body.innerHTML;
+        //   var newHTML = wholeHTML.replace(skillInReg, replaceText);
+        //   if(wholeHTML !== newHTML)
+        //     document.body.innerHTML = newHTML;
+        // }
+        // for (let listIndex = 0; listIndex < userSkillsList.length; listIndex ++){
+        //     skillInReg = new RegExp("\\b"+userSkillsList[listIndex]+ "\\b",'gi');
+        //     userSkillsCount += (wholeText.match(skillInReg) || []).length;
+        // }
+        //
+        // console.log("------skillsCount:");
+        // console.log(skillsCount);
+        // console.log("------userSkillsCount:");
+        // console.log(userSkillsCount);
+        //
+        // break;
       default:
         console.log('invalid request command');
         break;
--- popup.js
+++ popup.js
@@ -1,14 +1,16 @@
 let button = document.getElementById('search');
 
 button.onclick = function(element){
-  console.log('button pressed');
-
   chrome.tabs.query(
     {active: true, currentWindow: true},
     function(tabs) {
       chrome.tabs.sendMessage(
         tabs[0].id,
-        {command: "findAllSkills",keywords:['a']},
+        {
+          command: "findAllSkills",
+          allSkills:['JavaScript','question'],
+          userSkills:['question']
+        },
         function(response) {
           //do something with the response
         }
